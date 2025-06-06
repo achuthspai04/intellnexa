bf5f335e469ddb5e29cb57fefb0ae1c15bf7938c
Achuth S Pai <84560724+achuthspai04@users.noreply.github.com>
2025-06-06 22:15:13 +0530

    navbar complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,31 +1,36 @@
-import react from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState('Home');
 
-    return (
-        <>
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+
+  return (
+    <>
       <div className='head'>
+        <div className='logo-head'>
+          <img className='img' src="./intellnexa.png" alt="Intellnexa Logo" />
+          <p>Intellnexa</p>    
+        </div>
+        
         <div className='Navbar'>
           <nav>
             <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Services</a></li>
+              <li><a href="#" className={activeLink === 'Home' ? 'active' : ''} onClick={() => handleLinkClick('Home')}>Home</a></li>
+              <li><a href="#" className={activeLink === 'About' ? 'active' : ''} onClick={() => handleLinkClick('About')}>About Us</a></li>
+              <li><a href="#" className={activeLink === 'Services' ? 'active' : ''} onClick={() => handleLinkClick('Services')}>Services</a></li>
               <li>
                 <button type="button">Join Now</button>
               </li>
             </ul>
           </nav>
         </div>
-
-        <div className='logo-head'>
-          <img className='img' src="./intellnexa.png" alt="Intellnexa Logo" />
-          <p>Intellnexa</p>    
-        </div>
       </div>
     </>
-
-    );
+  );
 }
+
 export default Navbar;
---
+++ src/pages/foodprocessing.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import './foodprocessing.css';
+import Navbar from '../components/navbar';
+
+function FoodProcessing() {
+    return (
+        <>
+        <Navbar />
+        </>
+    );
+    }
