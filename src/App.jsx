a31b7f2a399380cbb701e9867a0d4ca840786c1d
Achuth S Pai <84560724+achuthspai04@users.noreply.github.com>
2025-06-08 23:10:59 +0530

    additional offerings started

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,48 @@ function App() {
 
       <div className='additional'>
         <p className='additional-head'>Additional Offerings</p>
-        
+
+        <div className='core-services'>
+          <div className='core1'>
+            <div className='content'>
+            <img className="core-image" src="/core1.svg" alt="add-on certificate course" width={90} height={90} />
+            <p className='core-header'>Add-on Certificate Courses</p>
+            <p className='core-text'>Skill-oriented short-term programs designed to complement academic learning.</p>
+            </div>
+          </div>
+
+          <div className='core2'>
+            <div className='content'>
+            <img className="core-image" src="/core2.svg" alt="workshops and seminars" width={90} height={90} />
+            <p className='core-header'>Workshops and Seminars</p>
+            <p className='core-text'> Interactive sessions covering emerging trends and practical applications across various fields</p>
+            </div>
+          </div>
+
+          <div className='core3'>
+            <div className='content'>
+            <img className="core-image" src="/core3.svg" alt="corporate training sessions" width={90} height={90} />
+            <p className='core-header'>Corporate Training Sessions</p>
+            <p className='core-text'>Customized programs to enhance employee productivity, communication, and team synergy</p>
+            </div>
+          </div>
+
+          <div className='core4'>
+            <div className='content'>
+            <img className="core-image" src="/core4.svg" alt="awareness sessions" width={90} height={90} />
+            <p className='core-header'>Awareness Sessions (online/offline)</p>
+            <p className='core-text'> Initiatives to spread awareness on mental health, nutrition, and wellness</p>
+            </div>
+          </div>
+
+          <div className='core4'>
+            <div className='content'>
+            <img className="core-image" src="/core4.svg" alt="awareness sessions" width={90} height={90} />
+            <p className='core-header'>Awareness Sessions (online/offline)</p>
+            <p className='core-text'> Initiatives to spread awareness on mental health, nutrition, and wellness</p>
+            </div>
+          </div>
+        </div>
         
       </div>
 
