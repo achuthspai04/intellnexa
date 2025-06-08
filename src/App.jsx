a7fea8f6fabc51a2c0c9f983403f6957c0d512c7
Achuth S Pai <84560724+achuthspai04@users.noreply.github.com>
2025-06-08 23:33:47 +0530

    additional offerings completed

--- src/App.jsx
+++ src/App.jsx
@@ -82,44 +82,44 @@ function App() {
       <div className='additional'>
         <p className='additional-head'>Additional Offerings</p>
 
-        <div className='core-services'>
-          <div className='core1'>
-            <div className='content'>
-            <img className="core-image" src="/core1.svg" alt="add-on certificate course" width={90} height={90} />
-            <p className='core-header'>Add-on Certificate Courses</p>
-            <p className='core-text'>Skill-oriented short-term programs designed to complement academic learning.</p>
+        <div className='additional-services'>
+          <div className='additional1'>
+            <div className='content1'>
+            <img className="addl-image" src="/addl1.svg" alt="Career Guidance and Counseling" width={90} height={90} />
+            <p className='addl-header'>Career Guidance and Counseling</p>
+            <p className='addl-text'> Personalized support for academic and career planning</p>
             </div>
           </div>
 
-          <div className='core2'>
-            <div className='content'>
-            <img className="core-image" src="/core2.svg" alt="workshops and seminars" width={90} height={90} />
-            <p className='core-header'>Workshops and Seminars</p>
-            <p className='core-text'> Interactive sessions covering emerging trends and practical applications across various fields</p>
+          <div className='additional2'>
+            <div className='content1'>
+            <img className="addl-image" src="/addl2.svg" alt="Project Support and Metorship" width={90} height={90} />
+            <p className='addl-header'>Project Support and Metorship</p>
+            <p className='addl-text'> Assistance with academic projects, research, and innovation incubation</p>
             </div>
           </div>
 
-          <div className='core3'>
-            <div className='content'>
-            <img className="core-image" src="/core3.svg" alt="corporate training sessions" width={90} height={90} />
-            <p className='core-header'>Corporate Training Sessions</p>
-            <p className='core-text'>Customized programs to enhance employee productivity, communication, and team synergy</p>
+          <div className='additional3'>
+            <div className='content1'>
+            <img className="addl-image" src="/addl3.svg" alt="Faculty Development Programs(FDPs)" width={90} height={90} />
+            <p className='addl-header'>Faculty Development Programs(FDPs)</p>
+            <p className='addl-text'> Designed for educators and trainers to upgrade their teaching methodologies</p>
             </div>
           </div>
 
-          <div className='core4'>
-            <div className='content'>
-            <img className="core-image" src="/core4.svg" alt="awareness sessions" width={90} height={90} />
-            <p className='core-header'>Awareness Sessions (online/offline)</p>
-            <p className='core-text'> Initiatives to spread awareness on mental health, nutrition, and wellness</p>
+          <div className='additional4'>
+            <div className='content1'>
+            <img className="addl-image" src="/addl4.svg" alt="Skill Development Bootcamps" width={90} height={90} />
+            <p className='addl-header'>Skill Development Bootcamps</p>
+            <p className='addl-text'> Fast-paced programs focused on soft skills and technical competencies</p>
             </div>
           </div>
 
-          <div className='core4'>
-            <div className='content'>
-            <img className="core-image" src="/core4.svg" alt="awareness sessions" width={90} height={90} />
-            <p className='core-header'>Awareness Sessions (online/offline)</p>
-            <p className='core-text'> Initiatives to spread awareness on mental health, nutrition, and wellness</p>
+          <div className='additional5'>
+            <div className='content1'>
+            <img className="addl-image" src="/addl5.svg" alt="Community Outreach Programs" width={90} height={90} />
+            <p className='addl-header'>Community Outreach Programs</p>
+            <p className='addl-text'>Educational drives and initiatives targeting social development</p>
             </div>
           </div>
         </div>
