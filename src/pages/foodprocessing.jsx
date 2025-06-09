a6267335f4e3082cd744f7f6b949aa64e3e897fc
Achuth S Pai <84560724+achuthspai04@users.noreply.github.com>
2025-06-10 01:41:41 +0530

    food block1 done

--- src/pages/foodprocessing.jsx
+++ src/pages/foodprocessing.jsx
@@ -9,7 +9,13 @@ function FoodProcessing() {
         <Navbar/>
             
             <div className='food1'>
-                <p className='food-title1'>Food Processing and Entrepreneurship Development  Programme </p>
+                <p className='food-title1'>Food Processing and Entrepreneurship Development Programme</p>
+                <div className='box1'>
+                    <p className='food-text1'>This comprehensive course in Food Processing and Entrepreneurship Development  Programme is designed to equip students with both theoretical knowledge and hands-on  experience across key areas of the food industry. It integrates core scientific principles with  modern innovations to prepare students for dynamic roles in food safety, quality control,  processing, and entrepreneurship.</p>
+                    <p className='food-text2'>The program is available in two formats: an add-on course and a diploma program </p>
+                    <p className='food-text3'>The <strong>add-on course</strong> is specially designed for students currently pursuing undergraduate or  postgraduate degrees in Food Science, Food Technology, Nutrition, Food Processing, and other  Bioscience fields. It aims to enhance their academic journey by providing practical exposure,  industry insights, and value-added national and international certifications alongside their  regular studies. </p>
+                    <p className='food-text4'>The <strong>diploma program</strong> provides comprehensive training in food processing technologies,  quality control, food safety, and entrepreneurship. It is ideal for individuals seeking specialized  knowledge and hands-on skills to enter the food industry or start their own food-related  ventures. This program equips learners with the practical competencies and credentials needed  for both employment and entrepreneurship in the growing food sector.</p>
+                </div>
             </div>
             
        <Footer/> 
