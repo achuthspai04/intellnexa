3da52eddce73d3337e214a30e14e6fc53f797aad
Achuth S Pai <84560724+achuthspai04@users.noreply.github.com>
2025-06-11 22:52:53 +0530

    desktop comeplted

--- src/pages/pyscho.jsx
+++ src/pages/pyscho.jsx
@@ -7,57 +7,57 @@ function Psycho() {
     return (
         <>
             <Navbar />
-            <div className='food1'>
-                <p className='food-title1'>Psychosocial support and Inclusive Counseling </p>
-                <div className='box1'>
-                    <p className='food-text1'>Psychosocial Support and Inclusive Counseling is an add-on course designed to equip learners with skills to address emotional, social, and psychological well-being in diverse populations. It emphasizes inclusive practices that respect cultural, gender, and ability differences. The course prepares participants to provide empathetic, accessible, and context-sensitive counseling support.</p>
-                    <p className='food-text2'>The program is available in two formats: an add-on course and a diploma program.</p>
-                    <p className='food-text2'>The <strong>add-on course</strong> in Psychosocial Support and Inclusive Counseling is designed as a supplementary program for students currently pursuing other academic degrees. It offers foundational knowledge and basic practical skills in counseling, enabling learners to integrate psychosocial awareness into their primary field of study. The course is ideal for those seeking an introduction to inclusive counseling without the commitment of a full-time program.</p>
-                    <p className='food-text2'>The <strong>certificate course or diploma version</strong> is a more intensive, standalone program aimed at graduates, professionals, or individuals seeking specialized training in the field. It provides deeper theoretical understanding, hands-on practice, and professional competency in psychosocial support and inclusive counseling. This format is suited for those looking to enhance their qualifications or pursue a career in mental health and counseling services.</p>
+            <div className='nfood1'>
+                <p className='nfood-title1'>Psychosocial support and Inclusive Counseling </p>
+                <div className='nbox1'>
+                    <p className='nfood-text1'>Psychosocial Support and Inclusive Counseling is an add-on course designed to equip learners with skills to address emotional, social, and psychological well-being in diverse populations. It emphasizes inclusive practices that respect cultural, gender, and ability differences. The course prepares participants to provide empathetic, accessible, and context-sensitive counseling support.</p>
+                    <p className='nfood-text2'>The program is available in two formats: an add-on course and a diploma program.</p>
+                    <p className='nfood-text2'>The <strong>add-on course</strong> in Psychosocial Support and Inclusive Counseling is designed as a supplementary program for students currently pursuing other academic degrees. It offers foundational knowledge and basic practical skills in counseling, enabling learners to integrate psychosocial awareness into their primary field of study. The course is ideal for those seeking an introduction to inclusive counseling without the commitment of a full-time program.</p>
+                    <p className='nfood-text2'>The <strong>certificate course or diploma version</strong> is a more intensive, standalone program aimed at graduates, professionals, or individuals seeking specialized training in the field. It provides deeper theoretical understanding, hands-on practice, and professional competency in psychosocial support and inclusive counseling. This format is suited for those looking to enhance their qualifications or pursue a career in mental health and counseling services.</p>
                 </div>
             </div>
             
 
-            <div className='learning'>
-                <p className='food-title2'>Learning Outcomes</p>
-                <div className='learning-box'>
-                    <div className='left'>
-                    <div className='lt'>
+            <div className='nlearning'>
+                <p className='nfood-title2'>Learning Outcomes</p>
+                <div className='nlearning-box'>
+                    <div className='nleft'>
+                    <div className='nlt'>
                         <img src='tick.svg' className='tick' alt='best bsc food processing in kochi'/>
-                        <p className='lt-text'>Differentiate between counseling, guidance, and psychotherapy.</p>
+                        <p className='nlt-text'>Differentiate between counseling, guidance, and psychotherapy.</p>
                     </div> 
 
-                    <div className='lt'>
+                    <div className='nlt'>
                         <img src='tick.svg' className='tick' alt='diploma in food processing in kerala'/>
-                        <p className='lt-text'>Identify the main goals and broad applications of counseling.</p>
+                        <p className='nlt-text'>Identify the main goals and broad applications of counseling.</p>
                     </div> 
 
-                    <div className='lt'>
+                    <div className='nlt'>
                         <img src='tick.svg' className='tick' alt='diploma in food processing'/>
-                        <p className='lt-text'>Understand the ethical principles that guide counseling practice.</p>
+                        <p className='nlt-text'>Understand the ethical principles that guide counseling practice.</p>
                     </div> 
 
-                    <div className='lt'>
+                    <div className='nlt'>
                         <img src='tick.svg' className='tick' alt='entrepreneurship courses in food technology kerala'/>
-                        <p className='lt-text'>Recognize the essential qualities of effective counselors and barriers to communication.</p>
+                        <p className='nlt-text'>Recognize the essential qualities of effective counselors and barriers to communication.</p>
                     </div>
 
-                    <div className='lt'>
+                    <div className='nlt'>
                         <img src='tick.svg' className='tick' alt='entrepreneurship courses in food technology kerala'/>
-                        <p className='lt-text'>Describe the stages, techniques, and adaptations used in counseling for special populations.</p>
+                        <p className='nlt-text'>Describe the stages, techniques, and adaptations used in counseling for special populations.</p>
                     </div>  
                     </div>
 
-                    <div className='right'>
+                    <div className='nright'>
                         <img src='psychology.svg' alt='coursees for psychology students in kerala'/>                   
                     </div>
                 </div>
 
-                <div className='giant'>
-                    <div className='giant-left'>
-                        <p className='left-header'>Core Learning Areas</p>
-                        <div className='left-box'>
-                            <ul className='core-areas-list'>
+                <div className='ngiant'>
+                    <div className='ngiant-left'>
+                        <p className='nleft-header'>Core Learning Areas</p>
+                        <div className='nleft-box'>
+                            <ul className='ncore-areas-list'>
                                 <li>Foundations of Counseling – Understanding definitions, ethics, stages, and communication in counseling.</li>
                                 <li>Counseling Techniques and Approaches – Learning key methods like person-centered, CBT, behavioral, feminist, and postmodern approaches.</li>
                                 <li>Applied Counseling Practice – Engaging in role plays, clinical visits, live sessions, and therapy workshops.</li>
@@ -67,13 +67,13 @@ function Psycho() {
                         </div>
                     </div>
 
-                    <div className='giant-right'>
-                        <p className='right-header'>Training & Certification Programs</p>
-                         <div className='right-box'>
-                            <div className='lb'>
-                                <div className='edath'>
-                                <p className='rtext'>
-                                    <ul className='rlist'>
+                    <div className='ngiant-right'>
+                        <p className='nright-header'>Training & Certification Programs</p>
+                         <div className='nright-box'>
+                            <div className='nlb'>
+                                <div className='nedath'>
+                                <p className='nrtext'>
+                                    <ul className='nrlist'>
                                         <li>Foundational Psychological Theories</li>
                                         <li>Core Counseling Skills Development</li>
                                         <li>Cognitive Behavioral Therapy (CBT) Workshops</li>
@@ -83,9 +83,9 @@ function Psycho() {
                                 </p>
                                 </div>
 
-                                <div className='valath'>
-                                    <p className='rtext'>
-                                    <ul className='rlist'>
+                                <div className='nvalath'>
+                                    <p className='nrtext'>
+                                    <ul className='nrlist'>
                                         <li>Live Counselling Sessions</li>
                                         <li>Role-Play Sessions</li>
                                         <li>Clinical Visits</li>
@@ -100,66 +100,31 @@ function Psycho() {
                 </div>
             </div>
 
-            <div className='lastt'>
-                <p className='last-header'>Job Sectors & Job Roles</p>
-                <div className='final-boxx1'>      
-                    <div className='top'>
-                        <p className='end-text'>This program provides learners with holistic training in human nutrition and dietetics,  combining scientific theory with real-world applications. Graduates will be prepared to  pursue careers in diverse nutrition-related fields or build independent practices in nutrition  counseling and wellness entrepreneurship.</p>
-                    </div> 
-
-                    <div className='bottom'>
-                        <div className='boxx1'>
-                            <p className='h'>Healthcare Institutions</p>
-                            <p className='t'>
-                                <ul className='end-l'>
-                                    <li>Diet Assistant</li>
-                                    <li>Nutrition Assistant</li>
-                                </ul>
-                            </p>
-                        </div>
-                        <div className='boxx1'>
-                            <p className='h'>Public Health & NGOs</p>
-                            <p className='t'>
-                                <ul className='end-l'>
-                                    <li>Community Nutritionist</li>
-                                    <li>Program Coordinator</li>
-                                </ul>
-                            </p>
-                        </div>
-
-                        <div className='boxx1'>
-                            <p className='h'>Food Service & Hospitality</p>
-                            <p className='t'>
-                                <ul className='end-l'>
-                                    <li>Diet Technician</li>
-                                    <li>Food Service Supervisor</li>
-                                    <li>Executive Nutritionist</li>
-                                </ul>
-                            </p>
-                        </div>
-
-                        <div className='boxx1'>
-                            <p className='h'>Fitness & Wellness Industry</p>
-                            <p className='t'>
-                                <ul className='end-l'>
-                                    <li>Health Coach</li>
-                                    <li>Wellness Coach</li>
-                                    
-                                </ul>
-                            </p>
-                        </div>
-
-                        <div className='boxx1'>
-                            <p className='h'>Entrepreneurship</p>
-                            <p className='t'>
-                                <ul className='end-l'>
-                                    <li>Nutrition Consultant</li>
-                                    <li>Health Blogger</li>
-                                    <li>Diet App Developer</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </div>   
+            <div className='nlastt'>
+                <p className='nlast-header'>Job Sectors & Job Roles</p>
+                <div className='nfinal-boxx1'> 
+                    <div className='nbig-beautiful-box'>   
+                    <p className='nfinaltext'>                        
+                        <ul className='nfinalrlist'>
+                            <li>School Counselor</li>
+                            <li>Mental Health Counselor</li>
+                            <li>Career Counselor</li>
+                            <li>Marriage and Family Therapist</li>
+                            <li>Substance Abuse Counselor</li>
+                            <li>Rehabilitation Counselor</li>
+                        </ul>
+                    </p>
+                    <p className='nfinaltext'>
+                        <ul className='nfinalrlist'>
+                            <li>Geriatric Counselor</li>
+                            <li>Sports Psychologist</li>
+                            <li>Organizational Consultant</li>
+                            <li>Researcher</li>
+                            <li>Teaching Field</li>
+                            <li>Non-profit organizations/NGO</li>
+                        </ul>
+                    </p>
+                    </div>
                 </div>
             </div>
             <Footer />
